edc846b66727f84ed0def0d8fd5c988acb9bf8c1
Darot <chendarot1@gmail.com>
2023-05-28 14:24:18 +0700

    Init Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app_challenge/config/theme_config.dart';
+import 'package:todo_app_challenge/firebase_options.dart';
 import 'package:todo_app_challenge/provider/todo_list_provider.dart';
 
 import 'view/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
